afe708a46d10668927310c2715fd324ef218bd7c
KulibabaYuliia <kulibaba120@gmail.com>
2024-01-17 01:19:56 +0200

    add id validation

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,20 +2,25 @@ const express = require("express");
 const router = express.Router();
 
 const contactsOpt = require("../../controllers/contacts.js");
+const isValidId = require("../../middlewares/isValidId.js");
+
 const jsonParser = express.json();
+
+// contactsOpt.listContacts;
 
 router.get("/", contactsOpt.listContacts);
 
-router.get("/:contactId", contactsOpt.getContactById);
+router.get("/:contactId", isValidId, contactsOpt.getContactById);
 
 router.post("/", jsonParser, contactsOpt.addContact);
 
-router.delete("/:contactId", contactsOpt.removeContact);
+router.delete("/:contactId", isValidId, contactsOpt.removeContact);
 
-router.put("/:contactId", jsonParser, contactsOpt.updateContact);
+router.put("/:contactId", isValidId, jsonParser, contactsOpt.updateContact);
 
 router.patch(
   "/:contactId/favorite",
+  isValidId,
   jsonParser,
   contactsOpt.updateFavoriteContact
 );
